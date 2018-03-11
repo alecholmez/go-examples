abc8dae3ea8d126f69806286fb7400f910b61144
Alec Holmes <alecholmez@me.com>
2018-03-11 18:54:46 -0400

    began implementation of binary search tree

---
+++ binsearchtree/node.go
@@ -0,0 +1,14 @@
+package main
+
+// Node - basis for binary search tree
+type Node struct {
+	value interface{}
+	Left  *Node
+	Right *Node
+}
+
+// Insert will inject a new node
+func (n *Node) Insert(value interface{}) error {
+
+	return nil
+}
---
+++ binsearchtree/tree.go
@@ -0,0 +1,11 @@
+package main
+
+// Tree ...
+type Tree struct {
+	Root *Node
+}
+
+// Insert will inject a new node into the binary search tree
+func (t *Tree) Insert(value interface{}) {
+
+}
